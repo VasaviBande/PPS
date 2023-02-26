f9b5ccd5e82fd794044586c171a18a4f71a79f85
VASAVI BANDE <30948618+VasaviBande@users.noreply.github.com>
2023-02-26 13:37:26 +0000

    bugfix

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int main(void) {
   char name[50];
   int i,len;
-  printf("\n enter any string");
+  printf("\n enter any string: ");
   scanf("%s",name);
   len=strlen(name);
   for(i=len;i>=0;i--)
     {
       int n=(int)name[i];
       //printf("%d",n);
-      if((n>97) && (n<122))
+      if((n>96) && (n<122))
       {
         printf("%c",name[i]);
         }
