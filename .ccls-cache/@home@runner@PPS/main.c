9da8029d25aaa6823fe666c43f60e27b7b1720d8
VASAVI BANDE <30948618+VasaviBande@users.noreply.github.com>
2023-02-26 13:26:24 +0000

    string reverse prg

--- .ccls-cache/@home@runner@PPS/main.c
+++ .ccls-cache/@home@runner@PPS/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-
+#include<string.h>
 int main(void) {
-  printf("Hello Students\n");
-  return 0;
+  char name[50];
+  int i,len;
+  printf("\n enter any string");
+  scanf("%s",name);
+  len=strlen(name);
+  for(i=len;i>=0;i--)
+    {
+      int n=(int)name[i];
+      //printf("%d",n);
+      if((n>97) && (n<122))
+      {
+        printf("%c",name[i]);
+        }
+    }
+  return 1;
 }
